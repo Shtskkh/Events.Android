697caed48ff0624b5338ffec0cb78ce071561210
Linar232 <anastasiya.mz03@mail.ru>
2026-03-06 11:32:31 +0700

    прототип дизайна главной страницы

--- app/src/main/java/com/events/app/ui/views/main/MainScreen.kt
+++ app/src/main/java/com/events/app/ui/views/main/MainScreen.kt
@@ -21,7 +21,8 @@ import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+
 import com.events.app.ui.components.eventscards.UpcomingEventCard
 
 @Composable
